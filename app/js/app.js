d37ce674a11caf26d04aec33ff389d7f8b2c6170
Mike Arvela <mike.arvela@futurice.com>
2015-03-22 08:27:59 +0200

    Make main .jsx a .js to enable build

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,9 @@
-/** @jsx React.DOM */
 import React from "react"
 import Router from "react-router"
 import Header from "./header.jsx!"
 import Content from "./content.jsx!"
 
 React.render(
-    <div>
-        <Header />
-        <Content />
-    </div>,
+    React.createElement("div", null, Header(), Content()),
     document.getElementById("main")
 )
