a54db88049c4ca1a086e3525e8b22e996324d9cf
Mike Arvela <mike.arvela@futurice.com>
2015-04-11 14:14:53 +0300

    Initialize some users and adventures

--- server/index.js
+++ server/index.js
@@ -1,27 +1,3 @@
 "use strict";
 
-var Sequelize = require("sequelize");
-var path = require("path");
-
-var sequelize = new Sequelize(null, null, null, {
-    dialect: "sqlite",
-    storage: path.resolve(".", "database.sqlite")
-});
-
-var User = sequelize.define("user", {
-    firstName: {
-        type: Sequelize.STRING
-    },
-    lastName: {
-        type: Sequelize.STRING
-    }
-}, {
-    freezeTableName: true
-});
-
-User.sync({ force: true }).then(function() {
-    return User.create({
-        firstName: "Frank",
-        lastName: "Ballston"
-    });
-});
+var db = require("./database");
