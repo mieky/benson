f80d0bcb1284d89ee6eecbf7c13db6cadba4c1c9
Mike Arvela <mike.arvela@futurice.com>
2015-04-25 08:29:03 +0300

    Serve static files with restify, prefix api paths

--- server/api.js
+++ server/api.js
@@ -14,22 +14,22 @@ function jsonOr404(req, res, next) {
 }
 
 export function initialize(server) {
-    server.get("/adventure/:id", (req, res, next) => {
+    server.get("/api/adventure/:id", (req, res, next) => {
         service.getAdventure(req.params.id)
             .then(jsonOr404(req, res, next));
     });
 
-    server.get("/adventure/:id/messages", (req, res, next) => {
+    server.get("/api/adventure/:id/messages", (req, res, next) => {
         service.getMessages(req.params.id)
             .then(jsonOr404(req, res, next));
     });
 
-    server.get("/user/:id", (req, res, next) => {
+    server.get("/api/user/:id", (req, res, next) => {
         service.getUser(req.params.id)
             .then(jsonOr404(req, res, next));
     });
 
-    server.get("/user", (req, res, next) => {
+    server.get("/api/user", (req, res, next) => {
         service.getUsersAsync()
             .then(jsonOr404(req, res, next));
     });
--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ let testdata = require("./testdata");
 let server = restify.createServer();
 require("./api").initialize(server);
 
+server.get(/\/?.*/, restify.serveStatic({
+    directory: "./app"
+}));
+
 testdata.createTestData()
     .then(() => {
         server.listen(8080, () =>
