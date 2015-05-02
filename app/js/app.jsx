005c07c93b562bbcedad9f288d4737cf9ac6a83f
Mike Arvela <mike.arvela@futurice.com>
2015-05-02 12:42:57 +0300

    Clean up

--- app/js/app.jsx
+++ app/js/app.jsx
@@ -9,115 +9,138 @@ let RouteHandler = Router.RouteHandler;
 let Link = Router.Link;
 let Route = Router.Route;
 
+function pretendRequest(email, pass, callback) {
+    setTimeout(() => {
+        if (email === "joe@example.com" && pass === "password1") {
+            callback({
+                authenticated: true,
+                token: Math.random().toString(36).substring(7)
+            });
+        } else {
+            callback({ authenticated: false });
+        }
+    }, 0);
+}
+
 var auth = {
-  login (email, pass, cb) {
-    cb = arguments[arguments.length - 1];
-    if (localStorage.token) {
-      if (cb) cb(true);
-      this.onChange(true);
-      return;
-    }
-    pretendRequest(email, pass, (res) => {
-      if (res.authenticated) {
-        localStorage.token = res.token;
-        if (cb) cb(true);
-        this.onChange(true);
-      } else {
-        if (cb) cb(false);
+    login(email, pass, callback) {
+        callback = arguments[arguments.length - 1];
+        if (localStorage.token) {
+            if (callback) {
+                callback(true);
+            }
+            this.onChange(true);
+            return;
+        }
+        pretendRequest(email, pass, (res) => {
+            if (res.authenticated) {
+                localStorage.token = res.token;
+                if (callback) {
+                    callback(true);
+                }
+                this.onChange(true);
+            } else {
+                if (callback) {
+                    callback(false);
+                }
+                this.onChange(false);
+            }
+        });
+    },
+
+    getToken: () => {
+        return localStorage.token;
+    },
+
+    logout: callback => {
+        delete localStorage.token;
+        if (callback) {
+            callback();
+        }
         this.onChange(false);
-      }
-    });
-  },
+    },
 
-  getToken: function () {
-    return localStorage.token;
-  },
+    loggedIn: () => {
+        return !!localStorage.token;
+    },
 
-  logout: function (cb) {
-    delete localStorage.token;
-    if (cb) cb();
-    this.onChange(false);
-  },
-
-  loggedIn: function () {
-    return !!localStorage.token;
-  },
-
-  onChange: function () {}
+    onChange: () => {}
 };
 
 class Login extends React.Component {
 
-  constructor() {
-    super();
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {
-      error: false
-    };
-  }
+    constructor() {
+        super();
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.state = {
+            error: false
+        };
+    }
 
-  handleSubmit (event) {
-    event.preventDefault();
-    var { router } = this.context;
-    var nextPath = router.getCurrentQuery().nextPath;
-    var email = this.refs.email.getDOMNode().value;
-    var pass = this.refs.pass.getDOMNode().value;
-    auth.login(email, pass, (loggedIn) => {
-      if (!loggedIn)
-        return this.setState({ error: true });
-      if (nextPath) {
-        router.replaceWith(nextPath);
-      } else {
-        router.replaceWith('/about');
-      }
-    });
-  }
+    handleSubmit(event) {
+        event.preventDefault();
+        var { router } = this.context;
+        var nextPath = router.getCurrentQuery().nextPath;
+        var email = this.refs.email.getDOMNode().value;
+        var pass = this.refs.pass.getDOMNode().value;
 
-  render () {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label><input ref="email" placeholder="email" defaultValue="joe@example.com"/></label>
-        <label><input ref="pass" placeholder="password"/></label> (hint: password1)<br/>
-        <button type="submit">login</button>
-        {this.state.error && (
-          <p>Bad login information</p>
-        )}
-      </form>
-    );
-  }
+        auth.login(email, pass, (loggedIn) => {
+            if (!loggedIn) {
+                return this.setState({ error: true });
+            }
+            if (nextPath) {
+                router.replaceWith(nextPath);
+            } else {
+                router.replaceWith("/");
+            }
+        });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label><input ref="email" placeholder="email" defaultValue="joe@example.com"/></label>
+                <label><input ref="pass" placeholder="password"/></label> (hint: password1)<br/>
+                <button type="submit">login</button>
+                {this.state.error && (
+                    <p>Bad login information</p>
+                )}
+            </form>
+        );
+    }
 }
 
 Login.contextTypes = {
-  router: React.PropTypes.func
+    router: React.PropTypes.func
 };
 
 class Logout extends React.Component {
-  componentDidMount () {
-    auth.logout();
-  }
+    componentDidMount () {
+        auth.logout();
+    }
 
-  render () {
-    return <p>You are now logged out</p>;
-  }
+    render () {
+        return <p>You are now logged out</p>;
+    }
 }
 
 class App extends React.Component {
     constructor() {
-        super()
+        super();
         this.state = {
             loggedIn: auth.loggedIn()
         };
     }
 
     setStateOnAuth (loggedIn) {
-      this.setState({
-        loggedIn: loggedIn
-      });
+        this.setState({
+            loggedIn: loggedIn
+        });
     }
 
     componentWillMount () {
-      auth.onChange = this.setStateOnAuth.bind(this);
-      auth.login();
+        auth.onChange = this.setStateOnAuth.bind(this);
+        auth.login();
     }
 
     render() {
@@ -140,32 +163,21 @@ class App extends React.Component {
 }
 
 var requireAuth = (Component) => {
-  return class Authenticated extends React.Component {
-    static willTransitionTo(transition) {
-      if (!auth.loggedIn()) {
-        transition.redirect('/login', {}, {'nextPath' : transition.path});
-      }
-    }
-    render () {
-      return <Component {...this.props}/>
-    }
-  }
+    return class Authenticated extends React.Component {
+        static willTransitionTo(transition) {
+            if (!auth.loggedIn()) {
+                transition.redirect("/login", {}, {
+                    "nextPath": transition.path
+                });
+            }
+        }
+        render() {
+            return <Component {...this.props} />;
+        }
+    };
 };
 
 let MessagesWithAuth = requireAuth(Messages);
-
-function pretendRequest(email, pass, cb) {
-  setTimeout(() => {
-    if (email === 'joe@example.com' && pass === 'password1') {
-      cb({
-        authenticated: true,
-        token: Math.random().toString(36).substring(7)
-      });
-    } else {
-      cb({authenticated: false});
-    }
-  }, 0);
-}
 
 let routes = (
     <Route name="app" path="/" handler={App}>
