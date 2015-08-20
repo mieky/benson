039df982cc73cc33056494da7c255d3a7a6e4834
Mike Arvela <mike.arvela@futurice.com>
2015-08-20 18:08:30 +0300

    Actually create some tables

--- server/database.js
+++ server/database.js
@@ -1,65 +1,137 @@
 import path from "path";
 import Promise from "bluebird";
 
-var dbFile = path.resolve(".", "knex.sqlite");
 var dbConfig = {
     client: "sqlite3",
     connection: {
-        filename: dbFile
-    }
+        filename: "./database.sqlite"
+    },
+    // debug: true
 };
 
-let knex = require("knex")(dbConfig);
-let bookshelf = require("bookshelf")(knex);
+var knex = require("knex")(dbConfig);
+var bookshelf = require("bookshelf")(knex);
 
 export function initialize(options) {
     options = options || {};
 
     console.log("Initializing database...");
 
-    let User = bookshelf.Model.extend({
+    // Schemas
+
+    let tables = [
+        bookshelf.knex.schema.hasTable("adventure").then(exists => {
+            if (!exists) {
+                return bookshelf.knex.schema.createTable("adventure", table => {
+                    table.increments("id").primary();
+                    table.string("name");
+                });
+            }
+        }),
+        bookshelf.knex.schema.hasTable("user").then(exists => {
+            if (!exists) {
+                return bookshelf.knex.schema.createTable("user", table => {
+                    table.increments("id").primary();
+                    table.string("first_name");
+                    table.string("last_name");
+                    table.string("email");
+                    table.string("image_url");
+                });
+            }
+        }),
+        bookshelf.knex.schema.hasTable("adventure_user").then(exists => {
+            if (!exists) {
+                return bookshelf.knex.schema.createTable("adventure_user", table => {
+                    table.integer("adventure_id").references("adventure.id");
+                    table.integer("user_id").references("user.id");
+                    table.timestamps();
+                });
+            }
+        }),
+        bookshelf.knex.schema.hasTable("message").then(exists => {
+            if (!exists) {
+                return bookshelf.knex.schema.createTable("message", table => {
+                    table.increments("id").primary();
+                    table.timestamp("created_at").defaultTo(knex.fn.now());
+                    table.string("text");
+                });
+            }
+        }),
+        bookshelf.knex.schema.hasTable("token").then(exists => {
+            if (!exists) {
+                return bookshelf.knex.schema.createTable("token", table => {
+                    table.timestamp("created_at").defaultTo(knex.fn.now());
+                    table.string("text");
+                });
+            }
+        })
+    ];
+
+    // Models
+
+    var User = bookshelf.Model.extend({
         tableName: "user",
-        adventures: () => {
+        adventures: function() {
             return this.belongsToMany(Adventure);
         },
-        messages: () => {
+        messages: function() {
             return this.hasMany(Message);
         },
-        tokens: () => {
+        tokens: function() {
             return this.hasMany(Token);
         }
     });
 
-    let Adventure = bookshelf.Model.extend({
+    var Adventure = bookshelf.Model.extend({
         tableName: "adventure",
-        messages: () => {
+        messages: function() {
             return this.hasMany(Message);
         },
-        users: () => {
-            return this.hasMany(User);
+        users: function() {
+            return this.belongsToMany(User);
         }
     });
 
-    let Message = bookshelf.Model.extend({
+    var Message = bookshelf.Model.extend({
         tableName: "message",
-        adventure: () => {
+        adventure: function() {
             return this.belongsTo(Adventure);
         },
-        user: () => {
+        user: function() {
             return this.belongsTo(User);
         }
     });
 
-    let Token = bookshelf.Model.extend({
+    var Token = bookshelf.Model.extend({
         tableName: "token",
-        user: () => {
+        user: function() {
             return this.belongsTo(User);
         }
     });
 
     if (options.createTestData) {
-        console.log("Creating test data...");
-    }
+        return Promise.all(tables)
+            .then(() => {
+                console.log("Creating test data...");
 
-    return Promise.resolve();
+                var adventure1 = new Adventure({ name: "A grand voyage "});
+                var user1 = new User({ "first_name": "Frank", "last_name": "Ballston" });
+                var user2 = new User({ "first_name": "Adam", "last_name": "Goggles" });
+
+                Promise.all([adventure1.save(), user1.save(), user2.save()])
+                    .then(() => {
+                        return adventure1.users().attach([user1, user2]);
+                    })
+                    .then(() => {
+                        Adventure.fetchAll({ withRelated: "users" }).then(advs => {
+                            advs.forEach(a => {
+                                console.log(a.toJSON());
+                            });
+                        });
+                    });
+            })
+            .catch(err => {
+                console.error(err);
+            });
+    }
 }
